304989704f0e0a66d4affd93e8df9aff048bfdc0
BihawaM <bihawam271@gmail.com>
2020-10-30 04:27:35 +0300

    initial commit

--- app/email.py
+++ app/email.py
@@ -1,6 +1,7 @@
 from flask_mail import Message
 from flask import render_template
 from . import mail
+
 def mail_message(subject,template,to,**kwargs):
     sender_email = 'bihawam271@gmail.com'
     email = Message(subject, sender=sender_email, recipients=[to])
